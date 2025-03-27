c248ed1c38ec9f961d22a7f666f26c5233f940c0
Nikita <nikidensor@gmail.com>
2025-03-27 09:48:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Win32;
+
+class Program
+{
+    static void Main()
+    {
+        using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion"))
+        {
+            if (key != null){
+                object installDateObj = key.GetValue("InstallDate");
+                try
+                {
+                    int installDateUnix = (int)installDateObj;
+                    DateTime installDate = DateTimeOffset.FromUnixTimeSeconds(installDateUnix).DateTime;
+                    Console.WriteLine($"Date of instalation: {installDate.Day:00}.{installDate.Month:00}.{installDate.Year}  {installDate.Hour:00}:{installDate.Minute:00}");
+                }
+                catch (Exception ex) {
+                
+                    Console.WriteLine("Error during fetching data");
+                }
+            }
+        }
+    }
+}
