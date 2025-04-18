9c1fb021b508a1e477f6f406caaf9505e3d412d8
Nikita <nikidensor@gmail.com>
2025-04-18 13:40:00 +0300

    +drives info

--- Program.cs
+++ Program.cs
@@ -13,15 +13,28 @@ class Program
                 {
                     int installDateUnix = (int)installDateObj;
                     DateTime installDate = DateTimeOffset.FromUnixTimeSeconds(installDateUnix).DateTime;
-                    Console.WriteLine($"OS: {Environment.OSVersion}\n");
+                    Console.WriteLine($"OS: {Environment.OSVersion}");
                     Console.WriteLine($"OS installation date: {installDate.Day:00}.{installDate.Month:00}.{installDate.Year}  {installDate.Hour:00}:{installDate.Minute:00}");
                     Console.WriteLine($"Machine name: {Environment.MachineName}");
-                    Console.WriteLine($"Is x64 system: {Environment.Is64BitOperatingSystem}\n");
-                    Console.ReadLine();
+                    Console.WriteLine($"Is x64 system: {Environment.Is64BitOperatingSystem}");
+                    Console.WriteLine($"System directory: {Environment.SystemDirectory}");
+                    Console.WriteLine($"Drives info:\n");
+                    var drives = System.IO.DriveInfo.GetDrives();
+                    for ( int i = 0; i < drives.Length; i++)
+                    {
+                        Console.WriteLine($"{i}. Name:{drives[i].Name}");
+                        Console.WriteLine($"Total size: {drives[i].TotalSize} bytes");
+                        Console.WriteLine($"Free space: {drives[i].TotalFreeSpace} bytes");
+                        Console.WriteLine($"Free space: {(double)drives[i].TotalFreeSpace / drives[i].TotalSize * 100:00.0} %");
+                        Console.WriteLine($"Type: {drives[i].DriveType}");
+                        Console.WriteLine($"Format: {drives[i].DriveFormat}");
+                        Console.WriteLine();
+                    }
+
                 }
                 catch (Exception ex) {
                 
-                    Console.WriteLine($"Error during fetching data: {ex}");
+                    Console.WriteLine($"Error during fetching data: {ex.Message}");
                 }
             }
         }
