7d31a4f572e26241a00282e660ee00191fab4006
Nikita <nikidensor@gmail.com>
2025-04-22 20:52:22 +0300

    Added program searcher feauture

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 class Program
 {
+    static List<SoftwareInfo> installedSoftware = new List<SoftwareInfo>();
     static void Main()
     {
         object installDateObj;
@@ -24,6 +25,7 @@ class Program
             Console.WriteLine($"OS: {Environment.OSVersion}");
             Console.WriteLine($"OS installation date: {installDate.Day:00}.{installDate.Month:00}.{installDate.Year}  {installDate.Hour:00}:{installDate.Minute:00}");
             Console.WriteLine($"Machine name: {Environment.MachineName}");
+            Console.WriteLine($"User name: {Environment.UserName}");
             Console.WriteLine($"Is x64 system: {Environment.Is64BitOperatingSystem}");
             Console.WriteLine($"System directory: {Environment.SystemDirectory}");
             Console.WriteLine($"Drives info:\n");
@@ -38,9 +40,18 @@ class Program
                 Console.WriteLine($"Format: {drives[i].DriveFormat}");
                 Console.WriteLine();
             }
-            Console.WriteLine($"Process architecture: {RuntimeInformation.ProcessArchitecture}");
+
+            installedSoftware.AddRange(GetInstalledSoftware(Registry.LocalMachine,
+     @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"));
+            Console.WriteLine("Installed programs:\nNAME - VERSION");
+            foreach (var software in installedSoftware)
+            {
+                Console.WriteLine($"{software.DisplayName} - {software.DisplayVersion}");
+            }
+            Console.WriteLine($"\nProcessor architecture: {RuntimeInformation.ProcessArchitecture}");
             Console.WriteLine($"Current framework: {RuntimeInformation.FrameworkDescription}");
             Console.WriteLine($"runtime identifier: {RuntimeInformation.RuntimeIdentifier}");
+            
         }
         catch (Exception ex)
         {
@@ -49,5 +60,46 @@ class Program
         }
 
     }
+   private  static List<SoftwareInfo> GetInstalledSoftware(RegistryKey registryKey, string registryPath)
+    {
+        List<SoftwareInfo> softwares = new List<SoftwareInfo>();
+
+        using (RegistryKey key = registryKey.OpenSubKey(registryPath))
+        {
+            if (key != null)
+            {
+                foreach (string subkeyName in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subkey = key.OpenSubKey(subkeyName))
+                    {
+                        try
+                        {
+
+
+                            string? displayName = subkey.GetValue("DisplayName")?.ToString();
+                            string displayVersion = subkey.GetValue("DisplayVersion").ToString();
+                            if (!string.IsNullOrEmpty(displayName))
+                            {
+                                softwares.Add(new SoftwareInfo
+                                {
+                                    DisplayName = displayName,
+                                    DisplayVersion = displayVersion
+                                });
+                            }
+                        }
+                        catch (Exception ) { }
+                        
+                    }
+                }
+            }
+        }
+        return softwares;
+    }
+}
+
+class SoftwareInfo
+{
+    public string DisplayName { get; set; }
+    public string DisplayVersion { get; set; }
 }
 
