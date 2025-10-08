b6e560a7dd3f82b1ed7d0078c3d4ebce71edf0b5
Murabe <85361539+MurabeDEV@users.noreply.github.com>
2025-10-08 19:38:00 +0300

    separate lines about os installation, drives and software info into separate funcs

--- Program.cs
+++ Program.cs
@@ -7,22 +7,10 @@ class Program
 
     static List<SoftwareInfo> installedSoftware = new List<SoftwareInfo>();
     static void Main()
-    {
-        object installDateObj;
-        using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion"))
-        {
-            if (key != null)
-            {
-                installDateObj = key.GetValue("InstallDate");
-            }
-            else
-            {
-                installDateObj = 0;
-            }
-        }
+    { 
         try
         {
-            int installDateUnix = (int)installDateObj;
+            int installDateUnix = (int)GetOSInstallDate();
 
             DateTime installDate = DateTimeOffset.FromUnixTimeSeconds(installDateUnix).DateTime;
             TimeSpan upTimeTS = TimeSpan.FromMilliseconds(Environment.TickCount64);
@@ -36,32 +24,11 @@ class Program
             Console.WriteLine($"System up time: {upTimeTS.Hours:00}:{upTimeTS.Minutes:00}:{upTimeTS.Seconds:00}");
 
             Console.WriteLine($"Drives info:\n");
-            var drives = System.IO.DriveInfo.GetDrives();
-            for (int i = 0; i < drives.Length; i++)
-            {
-                try
-                {
-                    Console.WriteLine($"{i}. Name: {drives[i].Name}");
-                    Console.WriteLine($"    Total size: {Tools.GetReadableStorageSize(drives[i].TotalSize)} ({drives[i].TotalSize} bytes)");
-                    Console.Write($"    Free space: {Tools.GetReadableStorageSize(drives[i].TotalFreeSpace)} ({drives[i].TotalFreeSpace} bytes)");
-                    Console.WriteLine($" {(double)drives[i].TotalFreeSpace / drives[i].TotalSize * 100:00.0} %");
-                    Console.WriteLine($"    Type: {drives[i].DriveType}");
-                    Console.WriteLine($"    Format: {drives[i].DriveFormat}");
-                    Console.WriteLine();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Problem with {i} drive: {e.Message}");
-                }
-            }
 
-            installedSoftware.AddRange(GetInstalledSoftware(Registry.LocalMachine,
-     @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"));
-            Console.WriteLine("Installed programs:\nNAME - VERSION");
-            foreach (var software in installedSoftware)
-            {
-                Console.WriteLine($"    {software.DisplayName} - {software.DisplayVersion}");
-            }
+            WriteDrivesInfo();
+
+            WriteInstalledSoftware();
+
             Console.WriteLine($"\nProcessor architecture: {RuntimeInformation.ProcessArchitecture}");
             Console.WriteLine($"Current framework: {RuntimeInformation.FrameworkDescription}");
             Console.WriteLine($"Runtime identifier: {RuntimeInformation.RuntimeIdentifier}");
@@ -75,7 +42,7 @@ class Program
         Console.WriteLine("\nPress ENTER to exit");
         Console.ReadLine();
     }
-    private static List<SoftwareInfo> GetInstalledSoftware(RegistryKey registryKey, string registryPath)
+    private static List<SoftwareInfo> GetInstalledSoftwareList(RegistryKey registryKey, string registryPath)
     {
         List<SoftwareInfo> softwares = new List<SoftwareInfo>();
 
@@ -109,6 +76,60 @@ class Program
             }
         }
         return softwares;
+    }
+
+    private static object GetOSInstallDate()
+    {
+        object installDateObj;
+        using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion"))
+        {
+            if (key != null)
+            {
+                installDateObj = key.GetValue("InstallDate");
+            }
+            else
+            {
+                installDateObj = 0;
+            }
+        }
+        if (installDateObj == null)
+        {
+            return 0;
+        }
+        return installDateObj;
+    }
+
+    private static void WriteDrivesInfo()
+    {
+        var drives = System.IO.DriveInfo.GetDrives();
+        for (int i = 0; i < drives.Length; i++)
+        {
+            try
+            {
+                Console.WriteLine($"{i}. Name: {drives[i].Name}");
+                Console.WriteLine($"    Total size: {Tools.GetReadableStorageSize(drives[i].TotalSize)} ({drives[i].TotalSize} bytes)");
+                Console.Write($"    Free space: {Tools.GetReadableStorageSize(drives[i].TotalFreeSpace)} ({drives[i].TotalFreeSpace} bytes)");
+                Console.WriteLine($" {(double)drives[i].TotalFreeSpace / drives[i].TotalSize * 100:00.0} %");
+                Console.WriteLine($"    Type: {drives[i].DriveType}");
+                Console.WriteLine($"    Format: {drives[i].DriveFormat}");
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Problem with {i} drive: {e.Message}");
+            }
+        }
+    }
+
+    private static void WriteInstalledSoftware()
+    {
+        installedSoftware.AddRange(GetInstalledSoftwareList(Registry.LocalMachine,
+    @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"));
+        Console.WriteLine("Installed programs:\nNAME - VERSION");
+        foreach (var software in installedSoftware)
+        {
+            Console.WriteLine($"    {software.DisplayName} - {software.DisplayVersion}");
+        }
     }
 }
 public class Tools
